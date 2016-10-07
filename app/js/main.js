c8297d0cca637ae18a4d9fb5ed35e165901aa3da
brightboahen <bridark17@gmail.com>
2016-10-07 11:30:47 +0100

    cwmbran webpage done

---
+++ app/js/main.js
@@ -0,0 +1,45 @@
+/**
+ * Created by bridark on 20/06/15.
+ */
+$(document).ready(function () {
+    //Contact form
+    $('#contactForm').submit(function (event) {
+        event.preventDefault();
+        var form = $(this);
+        $.ajax({
+            method:'POST',
+            url:'mail.php',
+            data:form.serialize(),
+            success:function(response){
+                if(response === 'true'){
+                    alert("Form successfully submitted, Thank you!");
+                    window.location.href = "index.html";
+                }else{
+                    alert("There was an error during submission please try again");
+                }
+            }
+        })
+    });
+    //Quote form
+    $('#quoteBtn').click(function (event) {
+        event.preventDefault();
+        var form = $("#quoteForm");
+        if(form[0].elements[0].value !=='' && form[0].elements[2].value !==''){
+            $.ajax({
+                method:'POST',
+                url:'mail.php',
+                data:form.serialize(),
+                success:function(response){
+                    if(response === 'true'){
+                        alert("Form successfully submitted, Thank you!");
+                        window.location.href = "index.html";
+                    }else{
+                        alert("There was an error during submission please try again");
+                    }
+                }
+            });
+        }else{
+            alert('Please fill out your name and phone number');
+        }
+    });
+});
